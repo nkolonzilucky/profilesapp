078118e9a36bfb0d3ee3bbba5cad6bb32e580db5
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-09-19 10:01:52 +0000

    added divs and played with styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <h1 className="flex flex-col space-y-1">
+      <div className='bg-black w-10 h-10 text-white'>1</div>
+      <div className='bg-slate-200 w-10 h-10'>2</div>
+      <div className='bg-gray-500 w-10 h-10'>3</div>
+      <div className='bg-red-200 w-10 h-10'>4</div>
+      <div className='bg-orange-200 w-10 h-10'>5</div>
     </h1>
   )
 }
